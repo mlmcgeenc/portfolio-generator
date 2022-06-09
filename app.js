a30724237ebfe80cff6116f322fc677913a70a7c
Matt McGee <mlmcgeenc@gmail.com>
2022-06-09 11:44:06 -0400

    create promises for writing html file and copying style sheet

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require('./src/page-template.js');
+const {writeFile, copyFile} = require('./utils/generate-site');
 
 const promptUser = () => {
 	return inquirer.prompt([
@@ -109,6 +109,12 @@ const promptProject = (portfolioData) => {
 			},
 			{
 				type: 'confirm',
+				name: 'featured',
+				message: 'Should this be a featured project?',
+				default: false,
+			},
+			{
+				type: 'confirm',
 				name: 'confirmAddProject',
 				message: 'Would you like to enter another project?',
 				default: false,
@@ -127,13 +133,18 @@ const promptProject = (portfolioData) => {
 promptUser()
 	.then(promptProject)
 	.then((portfolioData) => {
-		console.log(portfolioData);
+		return generatePage(portfolioData);
+	})
+	.then((pageHTML) => {
+		return writeFile(pageHTML);
+	})
+	.then((writeFileResponse) => {
+		console.log(writeFileResponse);
+		return copyFile();
+	})
+	.then((copyFileResponse) => {
+		console.log(copyFileResponse);
+	})
+	.catch((err) => {
+		console.log(err);
 	});
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('index.html', generatePage(name, github), (err) => {
-// 	if (err) throw err;
-
-// 	console.log('Portfolio complete! Check out indexedDB.html to see the output');
-// });
